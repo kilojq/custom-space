c98d0dc147038c32962172ccfcd88d8355bee258
kilojq <418291886@qq.com>
2018-12-23 22:10:12 +0800

    更新Demo示例文件

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 app.use('/', express.static(__dirname + "/dist"))
-app.set('views', './views')
+app.use('/demo', express.static(__dirname + "/Demo"))
 app.set('view engine', 'html')
 app.listen(9001, () => {
     console.log("node server is running at: http://localhost:9001")
+    console.log("Demo is running at: http://localhost:9001/demo")
 })
